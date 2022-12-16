664a8aea2498712e06bc8ef0bea4df73e69a83b5
Ulisses76 <aulissesn@gmail.com>
2022-12-16 15:15:19 -0300

    finished stock picker

---
+++ stock_picker/stock_picker.rb
@@ -0,0 +1,17 @@
+def stock_picker(day_prices)
+ max_profit = 0
+ result = Array.new
+  day_prices.each_with_index do |buy, buy_ind|
+    day_prices.each_with_index do |sell, sell_ind|
+      next if buy_ind > sell_ind
+      profit = sell - buy
+      if profit > max_profit 
+        max_profit = profit
+        result = [buy_ind, sell_ind]
+      end
+    end
+  end
+  result
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
